84626bef1909f5d3a7a5fbb98539d3e6db79eb28
JahanaviChaturvedi <118982130+JahanaviChaturvedi@users.noreply.github.com>
2025-03-26 19:35:33 +0530

    created a submission

--- script.js
+++ script.js
@@ -4,15 +4,14 @@ const openModal = document.getElementById('openModal');
 const closeModal = document.getElementById('closeModal');
 
 openModal.addEventListener('click', () => {
-    modal.style.display = 'flex';
+    modal.style.display = 'block';
 });
 
 closeModal.addEventListener('click', () => {
-    modal.style.display = 'none';
+    modal.style.display = 'none'; 
 });
-
 window.addEventListener('click', (event) => {
     if (event.target === modal) {
-        modal.style.display = 'none';
+        modal.style.display = 'none'; 
     }
 });
